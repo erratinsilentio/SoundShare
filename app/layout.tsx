e7d4f4a8f58c4d806086571f1d58ad327936df76
erratinsilentio <kacper.kondracki@wp.pl>
2024-11-15 13:14:23 +0100

    deleted unused imports in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Header } from "./components/layout/header";
-import { Footer } from "./components/layout/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
