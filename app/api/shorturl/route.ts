078f5ac16a42962cd2c454b7515653ec477f3307
erratinsilentio <kacper.kondracki@wp.pl>
2024-11-15 18:25:32 +0100

    deleted unused file

--- app/api/shorturl/route.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { nanoid } from "nanoid";
-import { NextRequest, NextResponse } from "next/server";
-
-export default async function handler(req: NextRequest, res: NextResponse) {
-  // generate new url with nanoid
-  // store it in db
-  // return short url to client
-}
